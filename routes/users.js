8e1f002d8d4b4213955e9ae2552b286b6adb94c6
Onel-R <leonreuben@yahoo.com>
2025-06-05 18:28:22 -0400

    Users API endpoints

--- routes/users.js
+++ routes/users.js
@@ -26,34 +26,59 @@ let users = [
 // GET request: Retrieve all users
 router.get("/",(req,res)=>{
   // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  res.send(JSON.stringify({users}, null, 4))//This line is to be replaced with actual return value
 });
 
 // GET by specific ID request: Retrieve a single user with email ID
 router.get("/:email",(req,res)=>{
   // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  const email = req.params.email;
+  res.send(users.filter(user => email === user.email))//This line is to be replaced with actual return value
 });
 
 
 // POST request: Create a new user
 router.post("/",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+    users.push({firstName, lastName, email, DOB} = req.query);
+    
+    // Copy the code here
+    res.send(users)//This line is to be replaced with actual return value
 });
 
 
 // PUT request: Update the details of a user by email ID
 router.put("/:email", (req, res) => {
   // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  let email = req.params.email;
+  let {firstName, lastName, DOB} = req.query
+  let filtered_users = users.filter(user => user.email === email)
+  let user = filtered_users[0]
+
+  if(firstName) {
+    user.firstName = firstName;
+  }
+  if(lastName) {
+    user.lastName = lastName;
+  }
+  if(DOB) {
+    user.DOB = DOB;
+  }
+  
+  users = users.filter(user => user.email != email);
+  users.push(user);
+
+  res.send(users)//This line is to be replaced with actual return value
 });
 
 
 // DELETE request: Delete a user by email ID
 router.delete("/:email", (req, res) => {
+    let index = users.findIndex(user => req.params.email === user.email);
+
   // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  users.splice(index, 1)
+
+  res.send(users)//This line is to be replaced with actual return value
 });
 
 module.exports=router;
